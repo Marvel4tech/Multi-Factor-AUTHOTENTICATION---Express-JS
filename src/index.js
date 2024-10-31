d553c95c931df86ffc3f51ae6bb65610d4af19be
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-10-31 18:17:19 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express, { urlencoded } from "express"
+import dotenv from "dotenv"
+import passport from "passport"
+import cors from "cors"
+import session from "express-session"
+
+dotenv.config()
+
+const app = express()
+
+// Middlewares
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credentials: true,
+}
+app.use(cors(corsOptions))
+app.use(express.json({ limit: "100mb" }))
+app.use(urlencoded({ limit: "100mb", extended: true}))
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    },
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Routes
+
+// Listen
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
