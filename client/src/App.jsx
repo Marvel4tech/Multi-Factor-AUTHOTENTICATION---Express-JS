0cb1aa05267b9c000ad088e3e19212be748b5a3a
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-06 16:38:10 +0100

    Project files and Folder structures is added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,9 @@ function App() {
       <h1 className=" text-3xl bg-green-600 text-white">
           Hello world
       </h1>
+      <p className=" bg-red-500 text-white">
+        Whats your name
+      </p>
     </>
   )
 }
---
+++ client/src/pages/Setup2fa.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Setup2fa = () => {
+  return (
+    <div>Setup2fa</div>
+  )
+}
+
+export default Setup2fa
