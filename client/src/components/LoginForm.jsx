e8ecb5716d8c3cc2e6d9ec4228cc07c30e56b3f9
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-08 01:52:25 +0100

    Handle Login and Register User is added

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,11 +1,57 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { register, loginUser } from '../service/authApi'
 
 const LoginForm = () => {
     const [isRegister, setIsRegister] = useState(false)
 
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [error, setError] = useState("")
+    const [message, setMessage] = useState("")
+
+    const handleLogin = async (e) => {
+        e.preventDefault()
+        try {
+            const {data} = await loginUser(username, password)
+            setMessage(data.message)
+            setUsername("")
+            setPassword("")
+        } catch (error) {
+            console.log("The err is : ", error.message)
+            setUsername("")
+            setPassword("")
+            setError("Invalid login credentials")
+        }
+    }
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        try {
+            const {data} = await register(username, password)
+            setIsRegister(false)
+            setMessage(data.message)
+            setUsername("")
+            setPassword("")
+            setConfirmPassword("")
+        } catch (error) {
+            console.log("The err is : ", error.message)
+            setUsername("")
+            setPassword("")
+            setConfirmPassword("")
+            setError("Something went wrong during user registration")
+        }
+    }
+
+    const handleRegisterToggle = () => {
+        setIsRegister(!isRegister)
+        setError("")
+        setMessage("")
+    }
+
   return (
-    <form className=' bg-white rounded-lg shadow-md w-full max-w-sm mx-auto'>
+    <form onSubmit={ isRegister ? handleRegister : handleLogin } className=' bg-white rounded-lg shadow-md w-full max-w-sm mx-auto'>
         <div className=' pt-6'>
             <h2 className=' text-3xl text-center font-extralight'>
                 {isRegister ? "Create Account" : "Login"}
@@ -23,8 +69,8 @@ const LoginForm = () => {
                         className=' w-full p-2 border rounded mt-2'
                         label='Username'
                         type='text'
-                        value=''
-                        onChange=''
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         placeholder='Enter Your Username'
                     />
                 </div>
@@ -36,8 +82,8 @@ const LoginForm = () => {
                         className=' w-full p-2 border rounded mt-2'
                         label='Password'
                         type='password'
-                        value=''
-                        onChange=''
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         placeholder='Enter Your Password'
                     />
                 </div>
@@ -50,22 +96,23 @@ const LoginForm = () => {
                             className=' w-full p-2 border rounded mt-2'
                             label='Confirm Password'
                             type='password'
-                            value=''
-                            onChange=''
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
                             placeholder='Enter Password Again'
                         />
                     </div>
                 ) : (
                   ""
                 )}
-                
+                {error && <p className=' text-red-500 text-sm mb-3'>{error}</p>}
+                {message && <p className=' text-green-600 text-sm mb-3'>{message}</p>}
                 <button className=' w-full bg-blue-500 text-white py-2 rounded-md' type='submit'>
                     {isRegister ? "Register" : "Login"}
                 </button>
                 <div>
                     <p className=' pt-4 text-center text-gray-600 text-sm'>
                         {isRegister ? "Already have an account ? " : "Don't have an account ? "} 
-                        <Link to="" onClick={() => setIsRegister(!isRegister)}>
+                        <Link to="" onClick={handleRegisterToggle}>
                             {isRegister ? "Login" : "Create Account"}
                         </Link>
                     </p>
