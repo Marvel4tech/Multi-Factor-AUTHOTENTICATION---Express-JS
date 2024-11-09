cbaa63133f524b21928e03be51bb7e9034de04ec
Marvel4tech <165481823+Marvel4tech@users.noreply.github.com>
2024-11-09 10:26:08 +0100

    Handle Setup 2FA API is added

--- client/src/components/TwoFASetup.jsx
+++ client/src/components/TwoFASetup.jsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
+import { setup2fa } from '../service/authApi'
 
-const TwoFASetup = () => {
+const TwoFASetup = ({ onSetupComplete }) => {
+    const [response, setResponse] = useState({})
     const [message, setMessage] = useState("")
 
+    const fetchQRCode = async () => {
+        const { data } = await setup2fa()
+        console.log(data)
+        setResponse(data)
+    }
+
+    useEffect(() => {
+        fetchQRCode()
+    }, [])
+
     const copyClipBoard = async () => {
-        await navigator.clipboard.writeText()
+        await navigator.clipboard.writeText(response.secret)
         setMessage("2FA code copied to clipboard")
     } 
 
   return (
-    <div className=' bg-white rounded-lg shadow-md w-full max-w-sm mx-auto'>
+    <div className=' bg-white rounded-lg shadow-md w-full max-w-sm mx-auto px-6'>
         <div className=' pt-6'>
             <h2 className=' text-3xl text-center font-extralight'>
                 Turn on 2FA verification
@@ -21,7 +33,7 @@ const TwoFASetup = () => {
             </p>
             <div className=' p-6'>
                 <div className=' flex justify-center'>
-                    <img src="" alt="2FA QR Code" className=' mb-4 border rounded-md' />
+                    <img src={response.qrImageUrl} alt="2FA QR Code" className=' mb-4 border rounded-md' />
                 </div>
                 <div className=' flex items-center mt-3 mb-3'>
                     <div className=' border-t border-2 border-gray-200 flex-grow'></div>
@@ -31,17 +43,17 @@ const TwoFASetup = () => {
                     <div className=' border-t border-2 border-gray-200 flex-grow'></div>
                 </div>
             </div>
-            <div className=' mb-6'>
+            <div className=' mb-6 px-6'>
                 { message && <p className=' text-green-600 text-sm mb-3'>{message}</p> }
                 <input 
                     readOnly
                     defaultValue=""
-                    value=""
+                    value={response.secret}
                     className=' w-full border rounded mt-2 text-xs text-gray-600 p-4'
                     onClick={copyClipBoard}
                 />
             </div>
-            <button onClick={onSetupComplete} className=' w-full bg-blue-500 text-white py-2 rounded-md'>
+            <button onClick={onSetupComplete} className=' w-full bg-blue-500 text-white py-2 rounded-md mb-6'>
                 Continue to Verification
             </button>
         </div>
